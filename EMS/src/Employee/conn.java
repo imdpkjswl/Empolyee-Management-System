d0998e5124699e4dc969afad384f96ddf91c3103
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-08 05:10:47 +0530

    Update conn.java

--- EMS/src/Employee/conn.java
+++ EMS/src/Employee/conn.java
@@ -1,5 +1,20 @@
 package Employee;
 
+/*
+CLASS Creation Steps:
+1.conn
+2.welcome_page
+3.login_page
+4.details_page
+5.add_employee
+6.view_employee
+7.print_data
+8.remove_employee
+9.search_employee
+10.update_employee
+
+*/
+
 import java.sql.*;
 
 public class conn {
